a98e1a46dfbd28ec318ddb38fdb3991786a8bd5e
Eduardo <eduardorireyes2024@gmail.com>
2023-04-10 11:02:10 -0600

    Rediseñe las barras de las secciones

--- src/components/Section_Right/index.jsx
+++ src/components/Section_Right/index.jsx
@@ -4,8 +4,9 @@ function SectionRight({nameSection, sectionSide}){
     return(
         <>
             <div className={`section-container section-container-${sectionSide}`}>
+                <span className={`design design-${'left'}`}></span>
                 <h2 className={`h2 h2-${sectionSide}`}>{nameSection}</h2>                         
-                <span className={`design design-${sectionSide}`}></span>
+                <span className={`design design-${'right'}`}></span>
             </div>
         </>
     );
