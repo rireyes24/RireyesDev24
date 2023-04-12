fb8a02b409d71012bba530cab27c0d63e21410d2
Eduardo <eduardorireyes2024@gmail.com>
2023-04-12 16:51:44 -0600

    Agregue el proyecto BreakingBadCharacters a mi portafolio

--- src/utils/dataProjects.js
+++ src/utils/dataProjects.js
@@ -4,27 +4,27 @@ const dataProjects = [
         name: 'WebGuitar',
         address: 'https://eduardorireyes24.github.io/WebGuitar/',
         image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116279/NewPortfolio2024/03_proyecto_ebhs6v.png',
-        description: 'Descripcion',
+        description: '',
 
     },
     {
         name: 'TokeiWatchWeb',
         address: 'https://eduardorireyes24.github.io/TokeiWatchWeb/',
         image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116279/NewPortfolio2024/02_proyecto_yvfmqu.png',
-        description: 'Descripcion',
+        description: '',
 
     },{
         name: 'Batatabit',
         address: 'https://eduardorireyes24.github.io/Batatabit_26-02-2022/',
         image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116277/NewPortfolio2024/01_proyecto_dtmx3q.png',
-        description: 'Descripcion',
+        description: '',
 
     },
     {
         name: 'BreakingBadCharacters',
-        address: '',
-        image: '',
-        description: 'Descripcion',
+        address: 'https://eduardorireyes24.github.io/BreakingBad_ER-140/',
+        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681339123/NewPortfolio2024/breaking-bad-characters_p8fntm.png',
+        description: '',
     },
 
 ];
