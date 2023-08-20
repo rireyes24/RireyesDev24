f469fed853a766751e10fd8092ca429136a4ac1d
Eduardo <eduardorireyes2024@gmail.com>
2023-08-19 22:23:00 -0600

    Agregue nuevas imagenes para mis proyectos

--- src/utils/dataProjects.js
+++ src/utils/dataProjects.js
@@ -3,7 +3,7 @@ const dataProjects = [
     {
         name: 'WebGuitar',
         address: 'https://eduardorireyes24.github.io/WebGuitar/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116279/NewPortfolio2024/03_proyecto_ebhs6v.png',
+        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692503748/NewPortfolio2024/WebGuitar_gj9lkc.png',
         description: 'Tu destino en línea para guitarras, accesorios y recursos de alta calidad. Explora WebGuitar hoy.',
         logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconWebGuitar_yfr2vp.svg',
 
@@ -11,14 +11,14 @@ const dataProjects = [
     {
         name: 'TokeiWatchWeb',
         address: 'https://eduardorireyes24.github.io/TokeiWatchWeb/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116279/NewPortfolio2024/02_proyecto_yvfmqu.png',
+        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504781/NewPortfolio2024/TokeiWatch_gcbblb.png',
         description: 'Tu fuente en línea de relojes de alta calidad. Encuentra el reloj perfecto en TokeiWatchWeb.',
         logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconTokai_qfcrs8.svg',
 
     },{
         name: 'Batatabit',
         address: 'https://eduardorireyes24.github.io/Batatabit_26-02-2022/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1680116277/NewPortfolio2024/01_proyecto_dtmx3q.png',
+        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504889/NewPortfolio2024/Batatabit_hgqoz1.png',
         description: '"Intercambio de criptomonedas seguro y conveniente. Compra, vende y intercambia criptomonedas en Batatabit hoy.',
         logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconBatata_zq0a6x.svg',
 
@@ -26,7 +26,7 @@ const dataProjects = [
     {
         name: 'BreakingBadCharacters',
         address: 'https://eduardorireyes24.github.io/BreakingBad_ER-140/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681339123/NewPortfolio2024/breaking-bad-characters_p8fntm.png',
+        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504604/NewPortfolio2024/BreakingBadChracters_jvpu6e.png',
         description: 'Descubre los personajes de Breaking Bad. Información detallada de los protagonistas de una de las mejores series del mundo.',
         logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511158/NewPortfolio2024/iconBB_iw7x5c.svg',
     },
