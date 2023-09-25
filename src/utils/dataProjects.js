27201fa0a56af594e666cbd449b15ebf258fc83a
Eduardo <eduardorireyes2024@gmail.com>
2023-09-24 22:28:53 -0600

    [Update] Agrego mi logo como icono y recorte de URLs en los datos del protecto (Necesito mandarlo atravez de una API -Strapi-)

--- src/utils/dataProjects.js
+++ src/utils/dataProjects.js
@@ -1,41 +1,44 @@
 
+const url = 'https://res.cloudinary.com/dejj8n6g7/image/upload/';
+
+
 const dataProjects = [    
     {
         name: 'ShopiReact',
         address: 'https://shopi-react-24.netlify.app/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1695444982/NewPortfolio2024/ShopiReact_sgqnzw.png',
+        image: `${url}v1695444982/NewPortfolio2024/ShopiReact_sgqnzw.png`,
         description: 'Tienda Online creada con React (Esta Tienda tendra una mejor version llamada "Polaris Store")',
-        logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconWebGuitar_yfr2vp.svg',
+        logo: `${url}v1695615431/NewPortfolio2024/logoRireyesTech_dxnyal.svg`,
     },
     {
         name: 'WebGuitar',
         address: 'https://eduardorireyes24.github.io/WebGuitar/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692503748/NewPortfolio2024/WebGuitar_gj9lkc.png',
+        image: `${url}v1692503748/NewPortfolio2024/WebGuitar_gj9lkc.png`,
         description: 'Tu destino en línea para guitarras, accesorios y recursos de alta calidad. Explora WebGuitar hoy.',
-        logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconWebGuitar_yfr2vp.svg',
+        logo: `${url}v1681511157/NewPortfolio2024/iconWebGuitar_yfr2vp.svg`,
 
     },
     {
         name: 'TokeiWatchWeb',
         address: 'https://eduardorireyes24.github.io/TokeiWatchWeb/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504781/NewPortfolio2024/TokeiWatch_gcbblb.png',
+        image: `${url}v1692504781/NewPortfolio2024/TokeiWatch_gcbblb.png`,
         description: 'Tu fuente en línea de relojes de alta calidad. Encuentra el reloj perfecto en TokeiWatchWeb.',
-        logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconTokai_qfcrs8.svg',
+        logo: `${url}v1681511157/NewPortfolio2024/iconTokai_qfcrs8.svg`,
 
     },{
         name: 'Batatabit',
         address: 'https://eduardorireyes24.github.io/Batatabit_26-02-2022/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504889/NewPortfolio2024/Batatabit_hgqoz1.png',
-        description: '"Intercambio de criptomonedas seguro y conveniente. Compra, vende y intercambia criptomonedas en Batatabit hoy.',
-        logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511157/NewPortfolio2024/iconBatata_zq0a6x.svg',
+        image: `${url}v1692504889/NewPortfolio2024/Batatabit_hgqoz1.png`,
+        description: 'Intercambio de criptomonedas seguro y conveniente. Compra, vende y intercambia criptomonedas en Batatabit hoy.',
+        logo: `${url}v1681511157/NewPortfolio2024/iconBatata_zq0a6x.svg`,
 
     },
     {
         name: 'BreakingBadCharacters',
         address: 'https://eduardorireyes24.github.io/BreakingBad_ER-140/',
-        image: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1692504604/NewPortfolio2024/BreakingBadChracters_jvpu6e.png',
+        image: `${url}v1692504604/NewPortfolio2024/BreakingBadChracters_jvpu6e.png`,
         description: 'Descubre los personajes de Breaking Bad. Información detallada de los protagonistas de una de las mejores series del mundo.',
-        logo: 'https://res.cloudinary.com/dejj8n6g7/image/upload/v1681511158/NewPortfolio2024/iconBB_iw7x5c.svg',
+        logo: `${url}v1681511158/NewPortfolio2024/iconBB_iw7x5c.svg`,
     },
 
 ];
