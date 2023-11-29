67110c46e7054b8b89e11e0c54375af34dc402f5
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2023-11-29 16:28:07 -0600

    [Add/Update/Delete] Añadi mi logo y quite GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RireyesDev24/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: () => 'never',
-      }
-    }
+  server: {
+    port: 1985,
+    open: true
   }
-  
 })
