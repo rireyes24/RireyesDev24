41d715e640dabb243d8ab43877a8452fcba0fb80
rireyes24 <eduardorireyes2024@gmail.com>
2024-03-07 15:50:41 -0600

    [Update] Cambie la url de mis proyectos que estan alojados en GitHub Pages

--- src/utils/dataProjects.js
+++ src/utils/dataProjects.js
@@ -26,7 +26,7 @@ const dataProjects = [
     },
     {
         name: 'WebGuitar',
-        address: 'https://eduardorireyes24.github.io/WebGuitar/',
+        address: 'https://rireyes24.github.io/WebGuitar/',
         image: `${url}v1692503748/NewPortfolio2024/WebGuitar_gj9lkc.png`,
         description: 'Tu destino en línea para guitarras, accesorios y recursos de alta calidad. Explora WebGuitar hoy.',
         logo: `${url}v1681511157/NewPortfolio2024/iconWebGuitar_yfr2vp.svg`,
@@ -34,14 +34,14 @@ const dataProjects = [
     },
     {
         name: 'TokeiWatchWeb',
-        address: 'https://eduardorireyes24.github.io/TokeiWatchWeb/',
+        address: 'https://rireyes24.github.io/TokeiWatchWeb/',
         image: `${url}v1692504781/NewPortfolio2024/TokeiWatch_gcbblb.png`,
         description: 'Tu fuente en línea de relojes de alta calidad. Encuentra el reloj perfecto en TokeiWatchWeb.',
         logo: `${url}v1681511157/NewPortfolio2024/iconTokai_qfcrs8.svg`,
 
     },{
         name: 'Batatabit',
-        address: 'https://eduardorireyes24.github.io/Batatabit_26-02-2022/',
+        address: 'https://rireyes24.github.io/Batatabit_26-02-2022/',
         image: `${url}v1692504889/NewPortfolio2024/Batatabit_hgqoz1.png`,
         description: 'Intercambio de criptomonedas seguro y conveniente. Compra, vende y intercambia criptomonedas en Batatabit hoy.',
         logo: `${url}v1681511157/NewPortfolio2024/iconBatata_zq0a6x.svg`,
@@ -49,7 +49,7 @@ const dataProjects = [
     },
     {
         name: 'BreakingBadCharacters',
-        address: 'https://eduardorireyes24.github.io/BreakingBad_ER-140/',
+        address: 'https://rireyes24.github.io/BreakingBad_ER-140/',
         image: `${url}v1692504604/NewPortfolio2024/BreakingBadChracters_jvpu6e.png`,
         description: 'Descubre los personajes de Breaking Bad. Información detallada de los protagonistas de una de las mejores series del mundo.',
         logo: `${url}v1681511158/NewPortfolio2024/iconBB_iw7x5c.svg`,
